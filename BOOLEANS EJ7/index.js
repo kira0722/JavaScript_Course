6faf4a153953fa636c4baf3ec72c88f2f3059bb5
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-05-08 12:29:36 -0500

    subiendo tipos de datos booleanos

---
+++ BOOLEANS EJ7/index.js
@@ -0,0 +1,18 @@
+const isActive = true;
+const hasPermission = false;
+
+//CONVERSION IMPLICITA
+
+const result = 5 > 3
+console.log(result);
+
+const name = 'name'
+console.log(!!name);
+
+
+//CONVERSION EXPLICITA
+
+const value = 0
+const otherValue = -12
+const explicitBoolean = Boolean(value)
+console.log(explicitBoolean);
