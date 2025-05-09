6712953dc101dc5aa8724c3645d959ff300d9fca
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-05-09 11:38:12 -0500

    subiendo operadores logicos

---
+++ EJ21 OPERADORES LOGICOS/index.js
@@ -0,0 +1,20 @@
+/* OPERADORES LOGICOS */
+
+/*
+&& //y
+|| //Y
+! //NO
+*/
+
+const a = 10
+const b = 20
+const c = "10"
+
+a == b && a == c
+
+a == b && a === c
+
+a != b || a === c
+
+!(a === c)
+
