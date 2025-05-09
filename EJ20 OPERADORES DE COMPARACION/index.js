67189a7ccb1a425a5254bb5adab5106e0a52674a
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-05-09 11:30:17 -0500

    subiendo operadores de comparacion

---
+++ EJ20 OPERADORES DE COMPARACION/index.js
@@ -0,0 +1,33 @@
+/* OPERADORES DE COMPARACION */
+ /*
+== //si es igual al valor
+=== //si es igual al valor y al tipo de dato
+
+!= //si es diferente a
+!== //es diferen a este valor // diferen en el valor y tipo de dato
+
+> //valor mayor a un valor
+< //valor menor a un valor
+
+>= //es mayor o igual a un valor
+<= //si es menor o igual a un valor
+*/
+
+const a = 10
+const b = 20
+const c = "10"
+
+a == b
+false
+a === c
+false
+a == c
+true
+a != b
+true
+a !== c
+true
+a > b
+false
+a <= b
+true
