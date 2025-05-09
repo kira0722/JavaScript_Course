ce3b5f1a7558f8838d1f08861b92617c34ffef43
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-05-09 11:49:20 -0500

    subiendo condicionales con if

---
+++ EJ22 CONDICIONAL IF/index.js
@@ -0,0 +1,18 @@
+//ESTRUCTURA
+// if (let === algo) {
+//     codigo
+// }else if(let === algomas){
+//     codigo
+// }else{
+
+// }
+
+let nombre = "diegos"
+
+if (nombre === "diego") {
+    console.log(`hola ${nombre}`);
+} else if (nombre === "diegos"){
+    console.log(`hola ${nombre}`);
+}else{
+    console.log("no encontrado");
+}
