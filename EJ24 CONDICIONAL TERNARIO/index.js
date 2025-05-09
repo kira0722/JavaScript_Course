b42c751ff3741dc008c069776d6fc510bcf2ff91
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-05-09 14:15:39 -0500

    subiendo condicional ternario

---
+++ EJ24 CONDICIONAL TERNARIO/index.js
@@ -0,0 +1,19 @@
+// let mensaje
+
+// const edad = 29
+
+// if (edad >= 18) {
+//     mensaje = "es mayor de edad"
+// }else{
+//     mensaje = "no es mayor de edad, no puede pasar";
+// }
+
+// console.log(mensaje);
+
+// condicion ? true : false
+
+const edad = 20
+
+const mensaje = edad >= 18 ? "es mayor de edad puede pasar" : "no es mayor de edad, no puede pasar"
+
+console.log(mensaje);
