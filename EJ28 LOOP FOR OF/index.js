09d0dca711084ccbc1a5a854ba259d489bfe5333
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-05-09 14:57:04 -0500

    subiendo loop de for of

---
+++ EJ28 LOOP FOR OF/index.js
@@ -0,0 +1,14 @@
+/* 
+solo funciona para iterables: arrays, strings [algo]
+for of
+
+for(variable of objeto){
+codigo
+}
+*/ 
+
+let canasta = ["manzana", "pera", "naranja", "uva"]
+
+for (fruta of canasta) {
+    console.log(fruta);
+}
