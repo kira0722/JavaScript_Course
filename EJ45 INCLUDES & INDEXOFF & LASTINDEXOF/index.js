b1fa59807b7c895f64a6f8a26c82e1123ee98598
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-05-22 15:57:29 -0500

    subiendo metodos includes & indexoff & lastindexoff

---
+++ EJ45 INCLUDES & INDEXOFF & LASTINDEXOF/index.js
@@ -0,0 +1,19 @@
+//includes  with numbers
+
+const numbers = [1, 2, 3, 4, 5, 6]
+
+const result1 = numbers.includes(5)
+console.log(result1);
+
+//IndexOf
+
+const fruits = ['apple', 'cherry', 'grape', 'mango']
+const index1 = fruits.indexOf('grapes2')
+console.log(index1);
+
+//lastIndexOf
+
+const numbersAgain = [2, 4, 6, 8, 10, 10]
+const lastIndex1 = numbersAgain.lastIndexOf(2)
+
+console.log(lastIndex1);
