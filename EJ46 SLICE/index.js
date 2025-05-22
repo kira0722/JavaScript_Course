d69e9383c7e575bf5d4d598814abc920544196c1
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-05-22 17:06:09 -0500

    subiendo metodo slice

---
+++ EJ46 SLICE/index.js
@@ -0,0 +1,14 @@
+//slice ()
+
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant']
+
+console.log(animals.slice(2));
+
+console.log(animals.slice(2, 4));
+console.log(animals.slice(1, 5));
+
+console.log(animals.slice(-2));
+
+console.log(animals.slice(2, -1));
+
+console.log(animals.slice());
