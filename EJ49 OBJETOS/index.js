099fa4661f7e21edcdc0133b86e196e974b81f8a
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-05-26 15:15:52 -0500

    subiendo segunda parte de objetos

--- EJ49 OBJETOS/index.js
+++ EJ49 OBJETOS/index.js
@@ -18,11 +18,28 @@ const persona = {
     edad: 30,
     direccion: {
         calle: "avenida saa",
-        ciudad: 'CDMX'
+        ciudad: 'CDMX',
     },
     saludar(){
-        console.log(`hola, mi nombre es `, persona.nombre);
-    }
-}
+        console.log(`hola, mi nombre es ${persona.nombre}`);
+    },
+};
 
 console.log(persona);
+console.log(persona.nombre);
+persona.saludar()
+
+persona.telefono = "555-555-5555";
+console.log(persona.telefono);
+
+persona.despedir = () => {
+    console.log("adios");
+}
+
+persona.despedir()
+
+console.log(persona.direccion.calle);
+
+delete persona.telefono;
+
+delete persona.despedir();
